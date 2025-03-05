3cfd072d4595d3924b67d165cc27c9a2575bfeb2
faldo <pn4066283@gmail.com>
2025-03-05 21:55:01 +0700

    done about us

--- src/components/AboutComponents.jsx
+++ src/components/AboutComponents.jsx
@@ -1,6 +1,10 @@
 // images
+import { Link } from "react-router-dom";
 import about1 from "../assets/images/about_1.jpg";
 import about2 from "../assets/images/about_2.jpg";
+
+// icons
+import { HiArrowNarrowRight } from "react-icons/hi";
 const AboutComponents = () => {
   const aboutContent = [
     {
@@ -14,6 +18,25 @@ const AboutComponents = () => {
       title:
         "To provide a seamless and professional website solution that enhances credibility, boosts conversions, and adapts to diverse.",
       images: about2,
+    },
+  ];
+
+  const countAbout = [
+    {
+      count: "92%",
+      title: "Happy Clients",
+    },
+    {
+      count: "24",
+      title: "Year of Experience",
+    },
+    {
+      count: "14.2k+",
+      title: "Completed Project",
+    },
+    {
+      count: "12+",
+      title: "Awards Won",
     },
   ];
   return (
@@ -52,15 +75,15 @@ const AboutComponents = () => {
         {aboutContent.map((item, i) => (
           <div
             key={i}
-            className="relative"
+            className="relative cursor-pointer group"
             data-aos="fade-up"
             data-aos-duration="1000"
           >
-            <div>
+            <div className="overflow-hidden rounded-xl">
               <img
                 src={item.images}
                 alt="about images"
-                className="rounded-xl h-72 lg:h-96 w-full object-cover "
+                className="rounded-xl h-72 lg:h-96 w-full object-cover group-hover:scale-110 ease-in-out duration-300"
               />
             </div>
             <div className="absolute bg-white dark:bg-gray-900 z-10 w-[95%] p-4 bottom-3 left-2/4 -translate-x-2/4 rounded-lg">
@@ -73,11 +96,25 @@ const AboutComponents = () => {
               <p className="text-gray-500 dark:text-gray-400 hidden lg:inline-block">
                 {item.title}
               </p>
+              <Link className="hidden items-center gap-3 text-blue-700 mt-4 group-hover:flex">
+                Learn More <HiArrowNarrowRight className="relative top-[1px]" />
+              </Link>
             </div>
           </div>
         ))}
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-10"></div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 md:gap-5">
+        {countAbout.map((item, i) => (
+          <div key={i} data-aos="fade-up" data-aos-duration="1000">
+            <div className="flex items-center justify-center flex-col gap-2 p-5 border border-gray-300 rounded-xl text-center cursor-pointer">
+              <h1 className="text-3xl font-semibold">
+                <span>{item.count}</span>
+              </h1>
+              <p className="text-gray-500 dark:text-gray-400">{item.title}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
