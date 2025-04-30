0ebb6e907aa0a4ce3e297908366e4eaeb7da25a4
faldo <pn4066283@gmail.com>
2025-04-30 13:48:15 +0700

    done blog 2

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -11,14 +11,18 @@ const Blog = () => {
       <CoverPage text={"Our Blogs"} />
       <section className="max-w-screen-xl m-auto p-4 grid lg:grid-cols-4 gap-5">
         <div className="lg:col-span-3 p-4 pt-8 border-2 rounded-xl border-gray-100 dark:border-gray-800 flex flex-col gap-10 relative mb-5">
-          <div className="absolute bg-white dark:bg-gray-900 py-2 px-6 rounded-lg -top-6 z-10 left-4 border-2 border-gray-100 dark:border-gray-800">
+          <div
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            className="absolute bg-white dark:bg-gray-900 py-2 px-6 rounded-lg -top-6 z-10 left-4 border-2 border-gray-100 dark:border-gray-800"
+          >
             <h1 className="text-gray-800 dark:text-white  font-medium text-lg">
               Latest Blogs
             </h1>
           </div>
           <div>
             {blogPosts.slice(0, 1).map((item, i) => (
-              <div key={i}>
+              <div key={i} data-aos="fade-up" data-aos-duration="1000">
                 <div className="grid lg:grid-cols-2 gap-5 lg:gap-10 items-center cursor-pointer group">
                   <div className="overflow-hidden rounded-lg shadow-lg">
                     <img
@@ -48,7 +52,7 @@ const Blog = () => {
           </div>
           <div className="grid lg:grid-cols-2 gap-10">
             {blogPosts.slice(1, 10).map((item, i) => (
-              <div key={i}>
+              <div key={i} data-aos="fade-up" data-aos-duration="1000">
                 <div className="flex flex-col gap-5 cursor-pointer">
                   <div className="overflow-hidden rounded-lg shadow-lg group">
                     <img
@@ -79,14 +83,18 @@ const Blog = () => {
         </div>
         <div className="grid md:grid-cols-2 lg:grid-cols-1 gap-10 h-max">
           <div className=" p-4 pt-8 border-2 rounded-xl border-gray-100 dark:border-gray-800 flex flex-col gap-10 relative h-max">
-            <div className="absolute bg-white dark:bg-gray-900 py-2 px-6 rounded-lg -top-6 z-10 left-4 border-2 border-gray-100 dark:border-gray-800">
+            <div
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              className="absolute bg-white dark:bg-gray-900 py-2 px-6 rounded-lg -top-6 z-10 left-4 border-2 border-gray-100 dark:border-gray-800"
+            >
               <h1 className="text-gray-800 dark:text-white text-base font-medium">
                 Popular
               </h1>
             </div>
             <div className="flex flex-col gap-5">
               {blogPosts.slice(5, 9).map((item, i) => (
-                <div key={i}>
+                <div key={i} data-aos="fade-up" data-aos-duration="1000">
                   <div className="grid grid-cols-2 gap-5 items-center">
                     <div>
                       <img
@@ -110,7 +118,11 @@ const Blog = () => {
             </div>
           </div>
           <div className=" p-4 pt-8 border-2 rounded-xl border-gray-100 dark:border-gray-800 flex flex-col gap-10 relative h-max">
-            <div className="absolute bg-white dark:bg-gray-900 py-2 px-6 rounded-lg -top-6 z-10 left-4 border-2 border-gray-100 dark:border-gray-800">
+            <div
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              className="absolute bg-white dark:bg-gray-900 py-2 px-6 rounded-lg -top-6 z-10 left-4 border-2 border-gray-100 dark:border-gray-800"
+            >
               <h1 className="text-gray-800 dark:text-white font-medium text-base">
                 Category
               </h1>
@@ -118,11 +130,9 @@ const Blog = () => {
             <div>
               {[...new Set(blogPosts.map((item) => item.category))].map(
                 (category, i) => (
-                  <div key={i}>
+                  <div key={i} data-aos="fade-up" data-aos-duration="1000">
                     <div className="flex items-center gap-4">
-                      <div className="w-2 h-2 bg-indigo-500 rounded-full">
-
-                      </div>
+                      <div className="w-2 h-2 bg-indigo-500 rounded-full"></div>
                       <h1 className="text-lg font-medium">
                         <span>{category}</span>
                       </h1>
